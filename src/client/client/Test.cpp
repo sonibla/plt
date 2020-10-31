a138042153923eaf2cf300f24128208aa05d2f66
maxime <maximemarroufin@gmail.com>
2020-10-31 09:18:04 +0100

    Basic Game ans Players creation

--- src/client/client/Test.cpp
+++ src/client/client/Test.cpp
@@ -4,12 +4,22 @@
 
 #include "Test.h"
 
+#include <iostream>
+
 using namespace client;
 using namespace state;
 
 
 
 void Test::state(){
-    std::weak_ptr<Game> game = Game::GetInstance();
+    std::vector<std::shared_ptr<Player>> _players;
+    _players.push_back(std::make_shared<Player>());
+    _players.push_back(std::make_shared<Player>());
+    
 
+    std::weak_ptr<Game> game = Game::GetInstance();
+    auto _game = game.lock();
+    _game->SetPlayers(_players);
+
+    std::cout << _game->GetPlayers().size() << std::endl;
 }
--- src/shared/state/Game.cpp
+++ src/shared/state/Game.cpp
@@ -4,6 +4,7 @@
 
 //using namespace state;
 #include "Game.h"
+
 using namespace state;
 using namespace std;
 
@@ -16,11 +17,35 @@ std::weak_ptr<Game> Game::GetInstance(){
     return instance;
 }
 void Game::NextTurn(std::weak_ptr<Player> player){}
-std::weak_ptr<Battlefield> Game::GetBattlefield(){}
-std::weak_ptr<Exile> Game::GetExile(){}
-std::weak_ptr<Stack> Game::GetStack(){}
-std::weak_ptr<Command> Game::GetCommand(){}
-std::vector<std::weak_ptr<Player>> Game::GetPlayers(){}
-Game::Game(){}
+std::weak_ptr<Battlefield> Game::GetBattlefield(){
+    return battlefield;
+}
+std::weak_ptr<Exile> Game::GetExile(){
+    return exile;
+}
+std::weak_ptr<Stack> Game::GetStack(){
+    return stack;
+}
+std::weak_ptr<Command> Game::GetCommand(){
+    return command;
+}
+std::vector<std::weak_ptr<Player>> Game::GetPlayers(){
+    std::vector<std::weak_ptr<Player>> _players;
+    for(size_t i=0;i<players.size();i++){
+        _players.push_back(players[i]);
+    }
+    return _players;
+}
+
+void Game::SetPlayers ( std::vector<std::shared_ptr<Player>> players){
+    this->players = players;
+}
+
+Game::Game (){
+    this->battlefield = std::make_shared<Battlefield>();
+    this->stack = std::make_shared<Stack>();
+    this->exile = std::make_shared<Exile>();
+    this->command = std::make_shared<Command>();
+}
 Game::~Game(){}
 
