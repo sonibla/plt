82ab7b605791d7614f4044d8ced3af392109d75b
maxime <maximemarroufin@gmail.com>
2020-11-15 21:12:09 +0100

    removed the build folder

--- build/test/client/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maxime/projects/plt/test/client
-# Build directory: /home/maxime/projects/plt/build/test/client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_client_dummy "/home/maxime/projects/plt/build/test/client/test_client_dummy")
-set_tests_properties(test_client_dummy PROPERTIES  _BACKTRACE_TRIPLES "/home/maxime/projects/plt/test/CMakeLists.txt;77;add_test;/home/maxime/projects/plt/test/client/CMakeLists.txt;3;add_custom_test;/home/maxime/projects/plt/test/client/CMakeLists.txt;0;")
