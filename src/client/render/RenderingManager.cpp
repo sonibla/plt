c42f962c265c32c87f397564c1232b88ca27181f
maxime <maximemarroufin@gmail.com>
2020-11-16 12:01:53 +0100

    creating window

---
+++ src/client/render/RenderingManager.cpp
@@ -0,0 +1,11 @@
+
+
+#include "RenderingManager.h"
+
+
+using namespace std;
+using namespace render;
+
+RenderingManager::RenderingManager(){
+    window.create(sf::VideoMode(1000, 1000), "Rendering");
+}
