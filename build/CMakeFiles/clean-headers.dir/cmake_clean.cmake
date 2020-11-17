3eb45bc7587fcdd18646836d678f653d0be62135
maxime <maximemarroufin@gmail.com>
2020-11-17 17:18:03 +0100

    revomed build/

--- build/CMakeFiles/clean-headers.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clean-headers"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clean-headers.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/run_test.sh
+++
@@ -1,2 +0,0 @@
-cd "/home/maxime/projetcs/plt/build/test"
-ctest --timeout 300 ||/bin/true
