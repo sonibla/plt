3989dc1f02b4c85c7cdf73aa2f07fd4476e5e304
quentin <quentin.chhean@ensea.fr>
2020-11-18 20:02:53 +0100

    adding permanentrenderer and battlefieldrenderer ccp and h files

--- src/client/render/PermanentRenderer.cpp
+++ src/client/render/PermanentRenderer.cpp
@@ -1,13 +1,19 @@
 #include "PermanentRenderer.h"
 
+using namespace std;
+using namespace render;
 
-PermanentRenderer::PermanentRenderer (state::Permanent permanent){
-    PermanentRenderer.setTexture(permanent.image_location)
-    if (permanent.tapped){
-        PermanentRenderer.setRotation(45);
+
+PermanentRenderer::PermanentRenderer (std::weak_ptr<state::Permanent> permanent){
+
+    std::shared_ptr<state::Permanent> _permanent = permanent.lock();
+    sf::Texture texture;
+    texture.loadFromFile(_permanent->image_location);
+    this->setTexture(texture);
+    if (_permanent->tapped){
+        this->setRotation(90);
     }
     else{
-        PermanentRenderer.setRotation(0);
+        this->setRotation(0);
     }
-    
 }
