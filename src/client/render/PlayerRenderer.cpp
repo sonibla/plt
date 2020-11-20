9371d64854b22f0bd66252db00bf27e18e7ae201
maxime <maximemarroufin@gmail.com>
2020-11-20 11:52:13 +0100

    Should be perfect now

--- src/client/render/PlayerRenderer.cpp
+++ src/client/render/PlayerRenderer.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 
 PlayerRenderer::PlayerRenderer(std::weak_ptr<state::Player> player){
-        this->setPosition(sf::Vector2f(10,690));
-    sf::Vector2f position = this->getPosition();
-    std::shared_ptr<state::Player> _player = player.lock();
-
     this->player=player;
+    std::shared_ptr<state::Player> _player = player.lock();
+    _player->addObserver(this);
+
+    this->setPosition(sf::Vector2f(10,690));
+    sf::Vector2f position = this->getPosition();
+    
     this->texture.loadFromFile("../res/textures/denim.png");
 
     this->sprite.setTexture(this->texture);
@@ -21,38 +23,26 @@ PlayerRenderer::PlayerRenderer(std::weak_ptr<state::Player> player){
     this->text.setCharacterSize(30);
     this->text.setPosition(position.x+10,position.y);
     this->text.setFont(this->font);
-    this->text.setString(std::string("Player \nLife :") + std::to_string(_player->life) + std::string("\nMana :") + std::to_string(_player->mana) + std::string("/")+std::to_string(_player->manaMax));
-
+    
     this->update((state::IObservable*) _player.get());
 }
-PlayerRenderer::~PlayerRenderer (){
 
+
+PlayerRenderer::~PlayerRenderer (){
+    this->player.lock()->removeObserver(this);
 }
+
+
 void PlayerRenderer::update (state::IObservable* obj){
 
+    this->text.setString(std::string("Player \nLife :") + std::to_string(_player->life) + std::string("\nMana :") + std::to_string(_player->mana) + std::string("/")+std::to_string(_player->manaMax));
+    this->notify();
 }
 
 
 
 void PlayerRenderer::draw (sf::RenderTarget &target, sf::RenderStates states) const{
     
-    int PlayerHP = 4;
-
-    sf::Texture hub_texture;
-    hub_texture.loadFromFile("../res/textures/denim.png");
-    sf::Font font;
-    font.loadFromFile("../res/fonts/arial.ttf");
-
-
-    sf::Sprite PlayerHub(hub_texture);
-    PlayerHub.setPosition(10,690);
-    PlayerHub.setScale(150/PlayerHub.getLocalBounds().width,300/PlayerHub.getLocalBounds().height);
-    
-    std::string PlayerString = std::string("Player \nHP :") + std::to_string(PlayerHP);
-    sf::Text PlayerHubText(PlayerString, font);
-    PlayerHubText.setCharacterSize(30);
-    PlayerHubText.setPosition(20,710);
-
     target.draw(this->sprite);
     target.draw(this->text);
 
