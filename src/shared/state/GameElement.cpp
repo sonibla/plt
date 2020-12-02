6f1f3ecd1f80152c71a560101dddaff58f63620c
maxime <maximemarroufin@gmail.com>
2020-12-02 15:57:22 +0100

    GameElement id system implementation

--- src/shared/state/GameElement.cpp
+++ src/shared/state/GameElement.cpp
@@ -13,25 +13,48 @@ int GameElement::GetID (){
 
 //Change the id of a gameElement , mostly when a gameElement move from a zone to another
 void GameElement::ChangeID (){
-    idTable.erase(this->GetID());
+    idTable.erase(this->id);
 
-    this->id = GameElement::NewID(std::make_shared<GameElement>(*this));
+    this->id = GameElement::NewID();
+
+    GameElement::Store(this->shared_from_this());
 }
 
-int GameElement::NewID (std::shared_ptr<GameElement> gameElement){
+int GameElement::NewID (){
     GameElement::lastID++;
-
-    idTable[ GameElement::lastID] = gameElement;
     return lastID;
+}
+void GameElement::Store(std::shared_ptr<GameElement> gameElement){
+
+    GameElement::idTable[gameElement->id] = gameElement->weak_from_this();
+}
+
+
+std::shared_ptr<GameElement> GameElement::Create(){
+    std::shared_ptr<GameElement> _gameElement = make_shared<GameElement>();
+
+    _gameElement->id = GameElement::NewID();
+    GameElement::Store(_gameElement);
+
+    std::cout << "created :" <<_gameElement->id << std::endl;
+    return _gameElement;
 }
 
 GameElement::GameElement(){
-    this->id = GameElement::NewID(std::make_shared<GameElement>(*this));
-
-    std::cout << "created :" <<this->id << std::endl;
 }
 
 GameElement::~GameElement(){
-    std::cout << "destroyed:" << this->id << std::endl;
-    idTable.erase(this->id);
+
+    if(this->id != -1){
+        std::cout << "destroying :" << this->id << std::endl;
+        GameElement::idTable.erase(GameElement::idTable.find(this->id));
+        /*auto finder = GameElement::idTable.find(this->id);
+        if(finder != GameElement::idTable.end()){
+            //GameElement::idTable.erase(finder);
+            std::cout << this->id <<" destroyed properly" << std::endl;
+        }
+        else{
+            std::cout << this->id <<" not found" << std::endl;
+        }*/
+    }
 }
