13aa4acc50b51295821d26879bd7172d4057c109
Alban Benmouffek <albanben@protonmail.com>
2020-12-15 14:29:40 +0100

    Add Serializable source code

---
+++ src/shared/state/Serializable.cpp
@@ -0,0 +1,13 @@
+#include "Serializable.h"
+
+using namespace std;
+using namespace state;
+
+Json::Value serialize() {
+	Json::Value emptyJson;
+	return emptyJson;
+}
+
+void deserialize(Json::Value json) {
+
+}
