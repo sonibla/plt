b75bad1f3f8a74c4feccaee9bf3eac39d1f21134
maxime <maximemarroufin@gmail.com>
2020-12-16 16:26:35 +0100

    added test to CMakeList and add empty execute and verify methods

--- src/shared/engine/CastCommand.cpp
+++ src/shared/engine/CastCommand.cpp
@@ -6,3 +6,11 @@ CastCommand::CastCommand(int cardID){
     this->id = CommandID::CAST;
     this->cardID = cardID;
 }
+
+void CastCommand::execute (std::shared_ptr<state::Game> game){
+
+}
+
+bool CastCommand::verify (std::shared_ptr<state::Game> game){
+
+}
