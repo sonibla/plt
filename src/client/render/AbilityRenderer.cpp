66302db05de96c0625d452ed5ca294b4943243b7
Quentin Chhean <ensea@StudentLab>
2020-12-16 15:55:28 +0100

    added controllerID in AbilityRenderer in render.dia ans implemented the AbilityRenderer.cpp

---
+++ src/client/render/AbilityRenderer.cpp
@@ -0,0 +1,21 @@
+#include "AbilityRenderer.h"
+
+using namespace std;
+using namespace render;
+
+
+AbilityRenderer::AbilityRenderer (std::weak_ptr<state::Ability> ability){
+
+    std::shared_ptr<state::Ability> _ability = ability.lock();
+    sf::Texture texture_load;
+    std::shared_ptr<state::Card> _source = std::dynamic_pointer_cast<state::Card>(_ability->source);
+    texture_load.loadFromFile(_source->image_location);
+    this->texture = texture_load;
+    this->setTexture(this->texture);
+    this->controllerID = _ability->controller;
+}
+
+
+AbilityRenderer::~AbilityRenderer (){
+
+};
