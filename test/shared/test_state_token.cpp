40adafa15c50844178e449d342d15cebab42733e
Alban Benmouffek <albanben@protonmail.com>
2020-12-21 11:49:12 +0100

    Added token test Actually it testes the serialization/deserialization process in the entire inheritance chain

--- test/shared/test_state_token.cpp
+++ test/shared/test_state_token.cpp
@@ -13,4 +13,20 @@ BOOST_AUTO_TEST_CASE(test_token_create){
     BOOST_TEST(token.get() != nullptr,"Token::Create create a non-Token object.");
 }
 
+BOOST_AUTO_TEST_CASE(test_token_serialize) {
+	std::shared_ptr<state::Token> token1 = std::dynamic_pointer_cast<Token>(Token::create());
+	std::shared_ptr<state::Token> token2 = std::dynamic_pointer_cast<Token>(Token::create());
+
+	token1->tapped = true;
+	token2->tapped = false; // They are different
+
+	token1->color = "1";
+	token2->color = "2";
+
+	token2->deserialize(token1->serialize());
+
+	BOOST_TEST(token1->tapped == token2->tapped);
+	BOOST_TEST(token1->color == token2->color); // Now they are equal
+}
+
 BOOST_AUTO_TEST_SUITE_END();
